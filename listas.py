42b553c6297560536110801ee605fd7cb48ea356
Agatha Cezar <agatha.cezar@gmail.com>
2023-09-27 11:53:28 -0300

    Listas dos usuários

---
+++ listas.py
@@ -0,0 +1,55 @@
+
+{
+  "id": 1,
+  "name": "Rebeca",
+  "account": {
+    "id": 1,
+    "email": "rebeca@gmail.com",
+    "nome de login": "Beca04",
+    "news": []
+  }
+}
+
+{
+  "id": 2,
+  "name": "Olivia",
+  "account": {
+    "id": 2,
+    "email": "olivia@gmail.com",
+    "nome de login": "Oli200",
+    "news": []
+  }
+}
+
+{
+  "id": 3,
+  "name": "Oliver",
+  "account": {
+    "id": 3,
+    "email": "oliver@gmail.com",
+    "nome de login": "Oliver00",
+    "news": []
+  }
+}
+
+{
+  "id": 4,
+  "name": "Carlos",
+  "account": {
+    "id": 4,
+    "email": "carlos@gmail.com",
+    "nome de login": "CarlosGol",
+    "news": []
+  }
+}
+
+{
+  "id": 5,
+  "name": "Julia",
+  "account": {
+    "id": 5,
+    "email": "julia@gmail.com",
+    "nome de login": "Julialima",
+    "news": []
+  }
+}
